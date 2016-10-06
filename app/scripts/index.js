5c1ec93896736af93022ef8449cb297f7e9fa0a1
Colin McKeon <14mckeon@gmail.com>
2016-10-06 15:17:15 -0400

    data being pulled form Github successfully

---
+++ app/scripts/index.js
@@ -0,0 +1,42 @@
+// console.log("Hello World!");
+//
+// alert('hey colin');
+
+
+var $ = require('jquery');
+var _ = require('underscore');
+var Handlebars = require('handlebars');
+
+
+var githubtoken = require('./gitapikey.js');
+
+if(githubtoken !== undefined){
+  $.ajaxSetup({
+    headers: {
+      'Authorization': 'token ' + githubtoken.token
+    }
+  })
+}
+
+$.ajax('https://api.github.com/users/colinmckeon').then(displayGit);
+
+
+function displayGit(data){
+  console.log(data);
+}
+
+
+
+
+
+
+//   var planets = data.results;
+//   var $planetContainer = $('#planet-list');
+//
+//   var source = $('#plaent-template').html();
+//   var template = Handlebars.compile(source);
+//
+//   _.each(planets, function(planet){
+//     $planetContainer.append(template(planet));
+//   });
+//
